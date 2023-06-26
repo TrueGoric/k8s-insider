a6c29e154c0ae76eb1f16980ab07b3d107e3a67a
Marcin Jędrasik <marcin.jedrasik@artifexidearum.net>
2023-06-27 01:38:49 +0200

    Moved FromStatus

--- app/k8s-insider-core/src/kubernetes/mod.rs
+++ app/k8s-insider-core/src/kubernetes/mod.rs
@@ -1,1 +1,17 @@
+use kube::core::object::HasStatus;
+
 pub mod operations;
+
+pub trait FromStatus<S> {
+    fn from_status(status: S) -> Self;
+}
+
+impl<T: Default + HasStatus<Status = S>, S> FromStatus<S> for T {
+    fn from_status(status: S) -> Self {
+        let mut object = Self::default();
+
+        *object.status_mut() = Some(status);
+
+        object
+    }
+}
--- app/k8s-insider-core/src/resources/mod.rs
+++ app/k8s-insider-core/src/resources/mod.rs
@@ -1,8 +1,7 @@
-use kube::{core::object::HasStatus, Resource};
 use thiserror::Error;
 
-pub mod controller;
 pub mod annotations;
+pub mod controller;
 pub mod crd;
 pub mod labels;
 pub mod router;
@@ -13,19 +12,5 @@ pub enum ResourceGenerationError {
     #[error("Provided dependent resource is missing a name!")]
     DependentMissingMetadataName,
     #[error("Provided dependent resource is missing a namespace!")]
-    DependentMissingMetadataNamespace
-}
-
-pub trait FromStatus<S> {
-    fn from_status(status: S) -> Self;
-}
-
-impl<T: Default + HasStatus<Status = S> + ?Sized, S> FromStatus<S> for T {
-    fn from_status(status: S) -> Self {
-        let mut object = Self::default();
-
-        *object.status_mut() = Some(status);
-
-        object
-    }
+    DependentMissingMetadataNamespace,
 }
