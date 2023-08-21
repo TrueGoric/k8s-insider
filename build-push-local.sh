7f0ac7dd75b4579e6b3fb0da401200da6ff41446
Marcin Jędrasik <marcin.jedrasik@artifexidearum.net>
2023-08-21 13:39:04 +0200

    devexp: Added install subcommand to build-push-local script

--- build-push-local.sh
+++ build-push-local.sh
@@ -14,4 +14,16 @@
     minikube image load k8s-insider-controller:latest --overwrite --daemon
     minikube image load k8s-insider-network-manager:latest --overwrite --daemon
     minikube image load k8s-insider-router:latest --overwrite --daemon
+
+    if [[ $1 = "install" ]]
+    then
+        cd app/k8s-insider || exit
+        cargo run -- install \
+            --controller-image k8s-insider-controller \
+            --controller-image-tag latest \
+            --network-manager-image k8s-insider-network-manager \
+            --network-manager-image-tag latest \
+            --router-image k8s-insider-router \
+            --router-image-tag latest
+    fi
 )
